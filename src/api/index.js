319b4e60c1914b9d5904fca09a709cdf3329e425
Vishal Manodra <vishal.manodra@github.com>
2026-06-15 11:21:32 +0530

    deployment

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,8 @@
 import axios from "axios";
 
 // Base URL of your backend — change this when you deploy
-const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const BASE_URL =
+  import.meta.env.VITE_API_URL || "https://ptc-portal-backend.onrender.com  ";
 
 // Create an axios instance with default settings
 // Every request made through "api" will automatically include these settings
